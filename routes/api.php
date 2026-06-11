98059615ca5e37eedde4782479af4c153ebd3e46
syahrulkhalik29-source <syahrulkhalik29@gmail.com>
2026-06-11 10:47:18 +0700

    Backend: MENYELESAIKAN SELURUH API CONTROLLER DAN ROUTE (Rekam Medis, Booking, Faskes, E-Resep)

--- routes/api.php
+++ routes/api.php
@@ -4,20 +4,25 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\RekamMedisController;
 use App\Http\Controllers\API\JadwalDokterController;
-use App\Http\Controllers\API\FaskesController; // <-- Tambah ini
+use App\Http\Controllers\API\FaskesController;
+use App\Http\Controllers\API\ResepObatController; // <-- Tambah ini untuk E-Resep
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// --- FITUR REKAM MEDIS ---
+// --- FITUR 1: REKAM MEDIS ---
 Route::get('/rekam-medis', [RekamMedisController::class, 'index']);
 Route::post('/rekam-medis', [RekamMedisController::class, 'store']);
 
-// --- FITUR BOOKING JADWAL DOKTER ---
+// --- FITUR 2: BOOKING JADWAL DOKTER ---
 Route::get('/jadwal-dokter', [JadwalDokterController::class, 'index']);
 Route::post('/jadwal-dokter', [JadwalDokterController::class, 'store']);
 
-// --- FITUR PENCARIAN FASKES TERDEKAT ---
+// --- FITUR 3: PENCARIAN FASKES TERDEKAT ---
 Route::get('/faskes', [FaskesController::class, 'index']);
 Route::post('/faskes', [FaskesController::class, 'store']);
+
+// --- FITUR 4: E-RESEP & DETAIL OBAT ---
+Route::get('/resep-obat', [ResepObatController::class, 'index']);
+Route::post('/resep-obat', [ResepObatController::class, 'store']);
