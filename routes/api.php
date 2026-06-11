1c2de8fdc6cf61f73d7137ce6a9a5ffaed9bcfb0
syahrulkhalik29-source <syahrulkhalik29@gmail.com>
2026-06-11 10:42:01 +0700

    Backend: Menambahkan Route API untuk Booking Jadwal Dokter

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\RekamMedisController;
+use App\Http\Controllers\API\JadwalDokterController; // <-- Jalur controller baru dimasukkan ke sini
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-use App\Http\Controllers\API\RekamMedisController;
-
+// --- FITUR REKAM MEDIS ---
 Route::get('/rekam-medis', [RekamMedisController::class, 'index']);
 Route::post('/rekam-medis', [RekamMedisController::class, 'store']);
+
+// --- FITUR BOOKING JADWAL DOKTER ---
+Route::get('/jadwal-dokter', [JadwalDokterController::class, 'index']);
+Route::post('/jadwal-dokter', [JadwalDokterController::class, 'store']);
